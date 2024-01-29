63734bc2ca76d4451cb646469fa0833cffa746cb
EliseAur <94222653+EliseAur@users.noreply.github.com>
2024-01-29 16:36:30 +0100

    Used npx prettier -w src/js/index.js to fix formatting errors

--- src/js/index.js
+++ src/js/index.js
@@ -13,43 +13,43 @@ import * as listeners from "./handlers/index.js";
  * setupRoutingAndListeners();
  */
 function setupRoutingAndListeners() {
-    listeners.setLogoutFormListener();
-    switch (location.pathname) {
-        case "/":
-        case "/index.html":
-            listeners.setLoginFormListener();
-            break;
-        case "/profile/register/":
-        case "/profile/register/index.html":
-            listeners.setRegisterFormListener();
-            break;
-        case "/posts/":
-        case "/posts/index.html":
-            listeners.renderPostsInFeed();
-            break;
-        case "/post/":
-        case "/post/index.html":
-            listeners.renderPostDetails();
-            break;
-        case "/profile/":
-        case "/profile/index.html":
-            listeners.renderProfileDetails();
-            listeners.renderPostsInFeed();
-            break;
-        case "/post/create/":
-        case "/post/create/index.html":
-            listeners.setCreatePostFormListener();
-            break;
-        case "/post/edit/":
-        case "/post/edit/index.html":
-            listeners.setUpdatePostFormListener();
-            break;
-        case "/profile/edit/":
-        case "/profile/edit/index.html":
-            listeners.setUpdateProfileFormListener();
-            break;
-        default:
-    }
+  listeners.setLogoutFormListener();
+  switch (location.pathname) {
+    case "/":
+    case "/index.html":
+      listeners.setLoginFormListener();
+      break;
+    case "/profile/register/":
+    case "/profile/register/index.html":
+      listeners.setRegisterFormListener();
+      break;
+    case "/posts/":
+    case "/posts/index.html":
+      listeners.renderPostsInFeed();
+      break;
+    case "/post/":
+    case "/post/index.html":
+      listeners.renderPostDetails();
+      break;
+    case "/profile/":
+    case "/profile/index.html":
+      listeners.renderProfileDetails();
+      listeners.renderPostsInFeed();
+      break;
+    case "/post/create/":
+    case "/post/create/index.html":
+      listeners.setCreatePostFormListener();
+      break;
+    case "/post/edit/":
+    case "/post/edit/index.html":
+      listeners.setUpdatePostFormListener();
+      break;
+    case "/profile/edit/":
+    case "/profile/edit/index.html":
+      listeners.setUpdateProfileFormListener();
+      break;
+    default:
+  }
 }
 
 setupRoutingAndListeners();
