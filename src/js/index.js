8b171bdd29ac006d82bb5539dcea1258c75a6949
EliseAur <94222653+EliseAur@users.noreply.github.com>
2024-01-30 16:46:05 +0100

    add more cases to the router in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -17,35 +17,50 @@ function setupRoutingAndListeners() {
   switch (location.pathname) {
     case "/":
     case "/index.html":
+    case "/workflow-css-frameworks/index.html":
       listeners.setLoginFormListener();
       break;
     case "/profile/register/":
     case "/profile/register/index.html":
+    case "/workflow-css-frameworks/profile/register/":
+    case "/workflow-css-frameworks/profile/register/index.html":
       listeners.setRegisterFormListener();
       break;
     case "/posts/":
     case "/posts/index.html":
+    case "/workflow-css-frameworks/posts/":
+    case "/workflow-css-frameworks/posts/index.html":
       listeners.renderPostsInFeed();
       break;
     case "/post/":
     case "/post/index.html":
+    case "/workflow-css-frameworks/post/":
+    case "/workflow-css-frameworks/post/index.html":
       listeners.renderPostDetails();
       break;
     case "/profile/":
     case "/profile/index.html":
+    case "/workflow-css-frameworks/profile/":
+    case "/workflow-css-frameworks/profile/index.html":
       listeners.renderProfileDetails();
       listeners.renderPostsInFeed();
       break;
     case "/post/create/":
     case "/post/create/index.html":
+    case "/workflow-css-frameworks/post/create/":
+    case "/workflow-css-frameworks/post/create/index.html":
       listeners.setCreatePostFormListener();
       break;
     case "/post/edit/":
     case "/post/edit/index.html":
+    case "/workflow-css-frameworks/post/edit/":
+    case "/workflow-css-frameworks/post/edit/index.html":
       listeners.setUpdatePostFormListener();
       break;
     case "/profile/edit/":
     case "/profile/edit/index.html":
+    case "/workflow-css-frameworks/profile/edit/":
+    case "/workflow-css-frameworks/profile/edit/index.html":
       listeners.setUpdateProfileFormListener();
       break;
     default:
